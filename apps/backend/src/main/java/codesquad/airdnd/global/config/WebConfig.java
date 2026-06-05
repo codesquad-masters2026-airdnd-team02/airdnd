015c049fa2afe99d745efa6de7d972e80a195c3a
LeeJaeWan <jjjkuul@naver.com>
2026-06-05 15:38:38 +0900

    feat: 프론트와 백엔드 CORS 문제 해결을 위한 WebConfig 구현 (#19)
    - localhost:5173 에서 들어온 요청 허용

--- apps/backend/src/main/java/codesquad/airdnd/global/config/WebConfig.java
+++ apps/backend/src/main/java/codesquad/airdnd/global/config/WebConfig.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import codesquad.airdnd.global.auth.LoginArgumentResolver;
@@ -18,4 +19,12 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
 		resolvers.add(loginArgumentResolver);
 	}
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowedHeaders("*");
+    }
 }
