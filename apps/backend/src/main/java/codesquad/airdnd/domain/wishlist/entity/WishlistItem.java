84f59b571482e94086b09de6022c4d2119a64f83
LeeJaeWan <jjjkuul@naver.com>
2026-06-08 23:31:08 +0900

    feat: /api/wishlists/{wishlistId} Entity 객체 구현
    - Wishlist와 Listing의 중간 엔티티인 WishlistItem은 PK가 복합키이나 복합키 외에 note, createdAt 이라는 컬럼을 가진 연관 엔티티
    - 따라서 @EmbeddedId 애너테이션을 사용하여 연관 엔티티 표현

---
+++ apps/backend/src/main/java/codesquad/airdnd/domain/wishlist/entity/WishlistItem.java
@@ -0,0 +1,42 @@
+package codesquad.airdnd.domain.wishlist.entity;
+
+import codesquad.airdnd.domain.listing.entity.Listing;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "wishlist_item")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class WishlistItem {
+
+    @EmbeddedId
+    private WishlistItemId id = new WishlistItemId();
+
+    @MapsId("wishlistId")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "wishlist_id", nullable = false)
+    private Wishlist wishlist;
+
+    @MapsId("listingId")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "listing_id", nullable = false)
+    private Listing listing;
+
+    private String note;
+
+    @Column(name = "created_at", insertable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Builder
+    protected WishlistItem(Wishlist wishlist, Listing listing, String note) {
+        this.wishlist = wishlist;
+        this.listing = listing;
+        this.note = note;
+    }
+}
