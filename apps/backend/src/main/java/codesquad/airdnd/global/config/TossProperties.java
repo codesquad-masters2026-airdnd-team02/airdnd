09beceb7a54a7d23ec58bf9e8f3146bd36f8b7b2
LeeJaeWan <jjjkuul@naver.com>
2026-06-15 18:07:05 +0900

    chore: Toss 결제 환경 설정 (#38)
    - application-local.yml: secret-key, base-url, success-url, fail-url 등록
    - TossProperties, AirdndApplication 에서 @ConfigurationProperties, @ConfigurationPRopertiesScan 을 통해 앱 전체에서 토스 관련 환경변수를 사용할 수 있도록 설정

---
+++ apps/backend/src/main/java/codesquad/airdnd/global/config/TossProperties.java
@@ -0,0 +1,8 @@
+package codesquad.airdnd.global.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "toss")
+public record TossProperties (
+        String secretKey, String baseUrl, String successUrl, String failUrl
+){}
