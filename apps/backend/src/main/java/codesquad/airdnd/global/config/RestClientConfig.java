4f0c881768a5341a72e8e8ffe113120cb2c4a7c3
LeeJaeWan <jjjkuul@naver.com>
2026-06-19 08:32:42 +0900

    RestClientconfig 주석 추가 (#44)

--- apps/backend/src/main/java/codesquad/airdnd/global/config/RestClientConfig.java
+++ apps/backend/src/main/java/codesquad/airdnd/global/config/RestClientConfig.java
@@ -25,6 +25,8 @@ public class RestClientConfig {
 
     @Bean
     public RestClient tossRestClient(TossProperties props, RestClient.Builder builder) {
+        // TODO: 추후 타임아웃/로깅/모니터링 구현
+        
         String basic = Base64.getEncoder()
                 .encodeToString((props.secretKey() + ":").getBytes(StandardCharsets.UTF_8));
 
