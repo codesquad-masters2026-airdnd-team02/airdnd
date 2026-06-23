51bee6b6a824da42555133382009bd06c03080ab
LeeJaeWan <jjjkuul@naver.com>
2026-06-23 09:36:59 +0900

    feat: 리스팅 id를 통한 wishistId 조회 구현 (#51)
    리스팅 목록 혹은 상세 페이지에서 위시리시트에 등록된 리스팅을 조회하는 메서드 구현
    - findWishlistId = 단건 조회 -> 상세 페이지에서 사용
    - findWishlistedPairs = 프로젝션을 이용해 WishlistedListing 리스트 반환

--- apps/backend/src/main/java/codesquad/airdnd/domain/wishlistItem/WishlistItemRepository.java
+++ apps/backend/src/main/java/codesquad/airdnd/domain/wishlistItem/WishlistItemRepository.java
@@ -1,13 +1,10 @@
 package codesquad.airdnd.domain.wishlistItem;
 
+import codesquad.airdnd.domain.wishlistItem.dto.query.WishlistedListing;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.*;
 
 public interface WishlistItemRepository extends JpaRepository<WishlistItem, WishlistItemId> {
 
@@ -18,12 +15,20 @@ public interface WishlistItemRepository extends JpaRepository<WishlistItem, Wish
     """)
     boolean existsByMemberIdAndListingId(Long memberId, Long listingId);
 
+    @Query("""
+        select wi.wishlist.id
+        from WishlistItem wi
+        where wi.wishlist.member.id = :memberId and wi.listing.id = :listingId
+    """)
+    Long findWishlistId(Long memberId, Long listingId);
+
     Optional<WishlistItem> findByWishlist_IdAndListing_Id(Long wishlistId, Long listingId);
 
 	@Query("""
-      	select wi.listing.id
-     	from WishlistItem wi
-    	where wi.wishlist.member.id = :memberId and wi.listing.id in :listingIds
+      	select new codesquad.airdnd.domain.wishlistItem.dto.query.WishlistedListing(
+	      	wi.listing.id, wi.wishlist.id)
+      	from WishlistItem wi
+        where wi.wishlist.member.id = :memberId and wi.listing.id in :listingIds
 	""")
-	Set<Long> findWishlistedListingIds(Long memberId, Collection<Long> listingIds);
+	List<WishlistedListing> findWishlistedPairs(Long memberId, Collection<Long> listingIds);
 }
